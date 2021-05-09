516679cd8c1b7e0c2373d65e9e10d4d91707a53d
alex <rp34789t5409yvt@protonmail.com>
2021-05-09 16:44:58 +0300

    Added moving of items in inventory

--- inventory.py
+++ inventory.py
@@ -24,8 +24,8 @@ class Inventory:
         self.__width = 480
         self.__height = 320
         self.__frame_thickness = 3
-        self.__inventory_rect = pygame.Rect(constants.GAME_WINDOW_WIDTH//2-self.__width//2,
-                                            constants.GAME_WINDOW_HEIGHT//2-self.__height//2,
+        self.__inventory_rect = pygame.Rect(constants.GAME_WINDOW_WIDTH // 2 - self.__width // 2,
+                                            constants.GAME_WINDOW_HEIGHT // 2 - self.__height // 2,
                                             self.__width, self.__height)
 
         self.__resources = {
@@ -42,6 +42,19 @@ class Inventory:
         self.__inventory_cells = list()
         self.__index_cell_to_add_new_resource = 0
 
+        self.__start_cell_to_move_item = 0
+        self.__end_cell_to_move_item = 0
+
+        self.__create_inventory_cells()
+
+    def __create_inventory_cells(self):
+        for x in range(constants.GAME_WINDOW_WIDTH // 2 - self.__width // 2,
+                       constants.GAME_WINDOW_WIDTH // 2 - self.__width // 2 + 12 * 40, 40):
+            for y in range(constants.GAME_WINDOW_HEIGHT // 2 - self.__height // 2,
+                           constants.GAME_WINDOW_HEIGHT // 2 - self.__height // 2 + 7 * 40, 40):
+                cell = pygame.Rect(x, y, 40, 40)
+                self.__inventory_cells.append(cell)
+
     def append_resource(self, resource_to_add):
         if resource_to_add.resource_name in self.__resources:
             self.__resources[resource_to_add.resource_name][1] += 1
@@ -51,20 +64,24 @@ class Inventory:
             self.__index_cell_to_add_new_resource += 1
 
     def draw_inventory(self, surface):
-        pygame.draw.rect(surface, constants.DARK_ORANGE_HIGHLIGHTED_MENU_ITEM,
-                         self.__inventory_rect, self.__frame_thickness)
+        def draw_outer_frame():
+            pygame.draw.rect(surface, constants.DARK_ORANGE_HIGHLIGHTED_MENU_ITEM,
+                             self.__inventory_rect, self.__frame_thickness)
 
-        for x in range(constants.GAME_WINDOW_WIDTH//2-self.__width//2,
-                       constants.GAME_WINDOW_WIDTH//2-self.__width//2 + 12*40, 40):
-            for y in range(constants.GAME_WINDOW_HEIGHT//2-self.__height//2,
-                           constants.GAME_WINDOW_HEIGHT//2-self.__height//2 + 7*40, 40):
-                cell = pygame.Rect(x, y, 40, 40)
-                pygame.draw.rect(surface, constants.TEST_COLOR, (x, y, 40, 40), self.__frame_thickness)
-                self.__inventory_cells.append(cell)
+        def draw_inventory_cells():
+            for cell in self.__inventory_cells:
+                x, y = cell.left, cell.top
+                pygame.draw.rect(surface, constants.TEST_COLOR, (x, y, 40, 40))
+                pygame.draw.rect(surface, (0, 0, 0), (x, y, 40, 40), self.__frame_thickness)
+
+        draw_outer_frame()
+        draw_inventory_cells()
 
         clock = pygame.time.Clock()
         is_inventory_showed = True
+        is_left_mouse_button_hold = False
         while is_inventory_showed:
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -73,10 +90,35 @@ class Inventory:
                     if event.key == pygame.K_i:
                         is_inventory_showed = False
 
+            clicked = pygame.mouse.get_pressed(3)
+            if clicked[0] and not is_left_mouse_button_hold:
+                start_coordinates = pygame.mouse.get_pos()
+                is_left_mouse_button_hold = True
+                for i in range(len(self.__inventory_cells)):
+                    if (self.__inventory_cells[i].left < start_coordinates[0] < self.__inventory_cells[i].right
+                            and
+                            self.__inventory_cells[i].top < start_coordinates[1] < self.__inventory_cells[i].bottom):
+                        self.__start_cell_to_move_item = i
+            if is_left_mouse_button_hold and not clicked[0]:
+                end_coordinates = pygame.mouse.get_pos()
+                is_left_mouse_button_hold = False
+                for i in range(len(self.__inventory_cells)):
+                    if (self.__inventory_cells[i].left < end_coordinates[0] < self.__inventory_cells[i].right
+                            and
+                            self.__inventory_cells[i].top < end_coordinates[1] < self.__inventory_cells[i].bottom):
+                        self.__end_cell_to_move_item = i
+                for resource in self.__resources.values():
+                    if resource[2] == self.__start_cell_to_move_item:
+                        pygame.draw.rect(surface, constants.TEST_COLOR, (self.__inventory_cells[resource[2]].left+3,
+                                                                         self.__inventory_cells[resource[2]].top+3,
+                                                                         34, 34))
+                        resource[2] = self.__end_cell_to_move_item
+
             self.__resources['gold_coin'][0].update(surface)
             self.__resources['silver_coin'][0].update(surface)
             self.__resources['bronze_coin'][0].update(surface)
 
+            pygame.display.flip()
             for name, resource in self.__resources.items():
                 if name != 'gold_coin' and name != 'silver_coin' and name != 'bronze_coin':
                     resource[0].rect.centerx = self.__inventory_cells[resource[2]].centerx
@@ -84,10 +126,9 @@ class Inventory:
                     surface.blit(resource[0].image, resource[0].rect)
                     Drawer.draw_text(surface, str(resource[1]), 20,
                                      constants.WHITE_COLOR_TITLE_BLOCKS,
-                                     self.__inventory_cells[resource[2]].centerx+10,
-                                     self.__inventory_cells[resource[2]].centery+5)
-
-            pygame.display.update()
+                                     self.__inventory_cells[resource[2]].centerx + 10,
+                                     self.__inventory_cells[resource[2]].centery + 5)
+            pygame.display.flip()
             clock.tick(constants.FPS_LOCKING)
 
     def get_key_for_chest(self):
